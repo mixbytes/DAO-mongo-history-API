a5d2537a0d65121dca5b04536e7d372edf2335cd
Sergey Govyazin <SGV123@yandex.ru>
2019-10-23 11:18:11 +0300

    [CYB-357] Config file renamed

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
+// CONFIG to be USED WITH docker-compose
+
 module.exports = {
     serverPort: 3333,
-    mongoURL: "mongodb://localhost:27017/EOS",
-    mongoDB:  "EOS",
-    chainUrl: "https://bp.cryptolions.io",
+    mongoURL: process.env.MONGO_URI,
+    chainUrl: process.env.NODE_URI,
     saveRequestsMetrics: true,
     maxSkip: 500000,
     maxTimeForOperetion: 60000, // 60sec
     clearOperationsTimer: 10000, // 10sec
-}
+};
